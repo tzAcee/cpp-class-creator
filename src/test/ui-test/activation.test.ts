28a1c714c7dda060b1d8dd035c0c778863a05e69
tzAcee <maximleis3@gmail.com>
2024-03-18 16:17:09 +0100

    init workflow

---
+++ src/test/ui-test/activation.test.ts
@@ -0,0 +1,22 @@
+import { assert } from 'console';
+import { VSBrowser, WebDriver } from 'vscode-extension-tester';
+
+describe('Activation test suite', () => {
+  let browser: VSBrowser;
+  let driver: WebDriver;
+  
+  before(async () => {
+    browser = VSBrowser.instance;
+    driver = browser.driver;
+  });
+
+  it('Extension can be activated by "Alt+X"', async () => {
+    console.log("here");
+    // Your test logic for Alt+X activation
+  });
+
+  it('Extension can be activated by the context menu', async () => {
+    console.log("here");
+    // Your test logic for context menu activation
+  });
+});
---
+++ src/test/ui-test/init.test.ts
@@ -0,0 +1,17 @@
+import { assert } from 'console';
+import { VSBrowser, WebDriver } from 'vscode-extension-tester';
+
+describe('Init test suite', () => {
+  let browser: VSBrowser;
+  let driver: WebDriver
+  
+  before(async () => {
+    browser = VSBrowser.instance;
+    driver = browser.driver;
+  });
+  
+  it('title not empty', async () => {
+    const title = await driver.getTitle();
+    assert(title != "");
+  });
+});
