16aca2f243a492d8fce6da66fb31bd59b34a401a
Maxim Leis <maxim.leis@siemens.com>
2024-03-22 15:07:04 +0100

    define all missing test cases

--- src/test/ui-test/replacement-commands.test.ts
+++ src/test/ui-test/replacement-commands.test.ts
@@ -38,7 +38,159 @@ describe('Text teplacement test suite', () => {
   beforeEach(async ()=>{
   })
 
-  it('Test', async () => {
+    // header file name tests:
+    it('[header file name] - CLASSNAMEUPPER multiple', async () => {
 
-  })
+    })
+
+    it('[header file name] - CLASSNAMEUPPER once', async () => {
+
+    })
+
+    it('[header file name] - CLASSNAMECAPI once', async () => {
+
+    })
+
+    it('[header file name] - CLASSNAME once', async () => {
+
+    })
+
+    it('[header file name] - CURRENTDATETIME once', async () => {
+
+    })
+
+    it('[header file name] - CURRENTDATE once', async () => {
+
+    })
+
+    it('[header file name] - CURRENTTIME once', async () => {
+
+    })
+
+    it('[header file name] - combination', async () => {
+
+    })
+
+    // source file name tests:
+    it('[source file name] - CLASSNAMEUPPER multiple', async () => {
+
+    })
+
+    it('[source file name] - CLASSNAMEUPPER once', async () => {
+
+    })
+
+    it('[source file name] - CLASSNAMECAPI once', async () => {
+
+    })
+
+    it('[source file name] - CLASSNAME once', async () => {
+
+    })
+
+    it('[source file name] - CURRENTDATETIME once', async () => {
+
+    })
+
+    it('[source file name] - CURRENTDATE once', async () => {
+
+    })
+
+    it('[source file name] - CURRENTTIME once', async () => {
+
+    })
+
+    it('[source file name] - combination', async () => {
+
+    })
+
+    // header file content tests:
+    it('[header file content] - HEADERFILENAME multiple', async () => {
+
+    })
+
+    it('[header file content] - HEADERFILENAME once', async () => {
+
+    })
+
+    it('[header file content] - SOURCEFILENAME once', async () => {
+
+    })
+
+    it('[header file content] - CLASSNAMEUPPER once', async () => {
+
+    })
+
+    it('[header file content] - CLASSNAMELOWER once', async () => {
+
+    })
+
+    it('[header file content] - CLASSNAMECAPI once', async () => {
+
+    })
+
+    it('[header file content] - CLASSNAME once', async () => {
+
+    })
+
+    it('[header file content] - CURRENTDATETIME once', async () => {
+
+    })
+
+    it('[header file content] - CURRENTDATE once', async () => {
+
+    })
+
+    it('[header file content] - CURRENTTIME once', async () => {
+
+    })
+
+    it('[header file content] - combination', async () => {
+
+    })
+
+    // source file content tests:
+    it('[source file content] - HEADERFILENAME multiple', async () => {
+
+    })
+
+    it('[source file content] - HEADERFILENAME once', async () => {
+
+    })
+
+    it('[source file content] - SOURCEFILENAME once', async () => {
+
+    })
+
+    it('[source file content] - CLASSNAMEUPPER once', async () => {
+
+    })
+
+    it('[source file content] - CLASSNAMELOWER once', async () => {
+
+    })
+
+    it('[source file content] - CLASSNAMECAPI once', async () => {
+
+    })
+
+    it('[source file content] - CLASSNAME once', async () => {
+
+    })
+
+    it('[source file content] - CURRENTDATETIME once', async () => {
+
+    })
+
+    it('[source file content] - CURRENTDATE once', async () => {
+
+    })
+
+    it('[source file content] - CURRENTTIME once', async () => {
+
+    })
+
+    it('[source file content] - combination', async () => {
+
+    })
 });
